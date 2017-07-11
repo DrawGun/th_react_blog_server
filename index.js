3455d8cb15300691b1fd861e6c4c19026f0e272b
Sergey Drogan <drawgun@yandex.ru>
2017-07-11 21:57:47 +0300

    поправил сервер

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ var application = express();
 var cors = require('cors');
 
 var items = require('./data').items;
+var maxItems = items.length
 
 var _ = require('lodash');
 
-application.use(cors());
+application.use(cors({
+  exposedHeaders:['MAX-POSTS'],
+}));
 
 application.get('/', function(req, res) {
-  res.json(items);
+  var posts = items;
+
+  if (req.query.query) {
+    posts = _.filter(items, function(i) { return i['title'].includes(req.query.query); });
+    maxItems = posts.length;
+  }
+
+  res.set({
+    'MAX-POSTS': maxItems
+  });
+
+  var step = parseInt(req.query.step);
+  var page = parseInt(req.query.page) - 1;
+  var arr_of_items = _.chunk(posts, step);
+  var res_items = arr_of_items[page];
+
+  res.json(res_items);
 });
 
 application.get('/posts/:id', function(req, res) {
